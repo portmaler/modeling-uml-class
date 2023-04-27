55d6d26a587a1b74a1200c5f846f3ae4eb046a0a
portmaler <56043999+portmaler@users.noreply.github.com>
2023-04-27 15:35:11 +0100

    correct springboot project

---
+++ springbootapp/src/test/java/com/sisbd/modeling/ModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisbd.modeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
